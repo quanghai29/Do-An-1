80a688367e1f3eb8e16bc5b39c18d689ab47a1e6
QUANG HAI <lequanghai540@gmail.com>
2019-11-11 20:39:38 +0700

    fix v7.4.1

--- 1712349-1712407/Contract.cs
+++ 1712349-1712407/Contract.cs
@@ -26,57 +26,10 @@ namespace _1712349_1712407
         public string To { get; set; }
 
         public event PropertyChangedEventHandler PropertyChanged;
-<<<<<<< HEAD
+
     }
 
-    //Move ISBN
-    public class ISBNArgs : StringArgs ,INotifyPropertyChanged
-    {
-        private string _direction;  // before/after
-
-        public string Direction
-        {
-            get => _direction;
-            set
-            {
-                _direction = value;
-                //NotifyChange("Direction");
-                NotifyChange("Description");
-            }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        public void NotifyChange(string v)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(v));
-        }
-=======
->>>>>>> 3545d6a... fix Clone for operation
-    }
-
-    //Move ISBN
-    public class ISBNArgs : StringArgs ,INotifyPropertyChanged
-    {
-        private string _direction;  // before/after
-
-        public string Direction
-        {
-            get => _direction;
-            set
-            {
-                _direction = value;
-                //NotifyChange("Direction");
-                NotifyChange("Description");
-            }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        public void NotifyChange(string v)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(v));
-        }
-    }
-
+   
     // Nhóm String Operation
     public abstract class StringOperation
     {
@@ -138,90 +91,6 @@ namespace _1712349_1712407
         }
     }
 
-    public class ISBNOperation : StringOperation, INotifyPropertyChanged
-    {
-        public override string Name => "Move ISBN";
-        public override string Description
-        {
-            get
-            {
-                var args = Args as ISBNArgs;
-                return $"Move ISBN '{args.Direction}' name";
-            }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        public override void Config()
-        {
-            var args = Args as ISBNArgs;
-            var screen = new ISBNDialog(args);
-            if (screen.ShowDialog() == true)
-            {
-                PropertyChanged?.Invoke(this,
-                    new PropertyChangedEventArgs("Description"));
-            }
-        }
-
-        public override string Operation(string Origin)
-        {
-            var isbnArgs = Args as ISBNArgs;
-            string ext = Path.GetExtension(Origin);
-            string newName = null;
-            if (isbnArgs.Direction == "before")
-            {
-                if(ext != "")
-                {
-                    string isbn = Origin.Substring(Origin.Length - ext.Length - 13, 13);
-                    string name = Origin.Substring(0, Origin.Length - isbn.Length - ext.Length);
-                    newName = $"{isbn}{name}{ext}";
-                }
-                else
-                {
-                    // khi file khong xac dinh hoac la folder
-                }
-            }
-            else if (isbnArgs.Direction == "after")
-            {
-               if (ext != "")
-               {
-                    string isbn = Origin.Substring(0, 13);
-                    string name = Origin.Substring(13, Origin.Length - isbn.Length - ext.Length);
-                    newName = $"{name}{isbn}{ext}";
-               }
-               else
-               {
-                    // khi file khong xac dinh hoac la folder
-               }
-            }
-            return Origin.Replace(Origin, newName);
-        }
-    }
-
-
-    public class UniqueOperation : StringOperation
-    {
-        public override string Name => "Unique name";
-        public override string Description
-        {
-            get
-            {
-                return "Generate unique name";
-            }
-        }
-
-        public override void Config()
-        {
-            throw new NotImplementedException();
-        }
-
-        public override string Operation(string Origin)
-        {
-            string ext = Path.GetExtension(Origin);
-            var g = Guid.NewGuid();
-            string newName = $"{g.ToString()}{ext}";
-            return Origin.Replace(Origin, newName);
-        }
-    }
 
 
     public abstract class StringName
