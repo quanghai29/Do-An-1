800dd60788f0b3b32336390958ab4e7858535995
LinhDong64 <mailinhdong@gmail.com>
2019-11-13 17:26:50 +0700

    Thêm vào NewCase method, Fullname  Normalize method

--- 1712349-1712407/Contract.cs
+++ 1712349-1712407/Contract.cs
@@ -30,6 +30,25 @@ namespace _1712349_1712407
 
     }
 
+    //NewCase
+    public class NewCaseArgs : StringArgs,INotifyPropertyChanged
+    {
+        //Kiểu NewCase;
+        public string Option { get; set; }
+
+        private void NotifyChange(string v)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(v));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    public class FullnameNormalize : StringArgs, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
     //Move ISBN
     public class ISBNArgs : StringArgs, INotifyPropertyChanged
     {
@@ -114,6 +133,155 @@ namespace _1712349_1712407
             var from = args.From;
             var to = args.To;
             return Origin.Replace(from, to);
+        }
+    }
+
+    //NewCase 
+    public class NewCaseOperation : StringOperation, INotifyPropertyChanged
+    {
+        public override string Name => "New Case";
+
+        public override string Description
+        {
+            get
+            {
+                var args = Args as NewCaseArgs;
+                if (args.Option == "")
+                {
+                    return "heLlO/HELLO/Hello/hello";
+                }
+                else
+                {
+                    return args.Option;
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public override StringOperation Clone()
+        {
+            var oldArgs = Args as NewCaseArgs;
+            return new NewCaseOperation()
+            {
+                Args = new NewCaseArgs()
+                {
+                    Option = oldArgs.Option
+                }
+            };
+        }
+
+        public override void Config()
+        {
+            var screen = new NewCaseConfigDialog(Args);
+            if (screen.ShowDialog() == true)
+            {
+                PropertyChanged?.Invoke(this,
+                    new PropertyChangedEventArgs("Description"));
+            }
+        }
+
+        public override string Operation(string Origin)
+        {
+            var args = Args as NewCaseArgs;
+            var option = args.Option;
+            var result = "";
+            if (option.Contains("Upper"))
+            {
+                result = Origin.ToUpper();
+            }
+            else if (option.Contains("Lower"))
+            {
+                result = Origin.ToLower();
+            }
+            else if (option.Contains("Cammel"))
+            {
+                var temp = new Support();
+                result = Origin.ToLower();
+                result = "_" + result;
+                for (int i = 0; i < result.Length - 1; i++)
+                {
+                    if (result[i] < 97 || result[i] > 122)
+                    {
+                        if (result[i + 1] >= 97 && result[i + 1] <= 122)
+                        {
+                            result = temp.MyReplace(result, i + 1);
+                            i++;
+                        }
+                    }
+                }
+                result = result.Substring(1, result.Length - 1);
+            }
+
+            return result;
+        }
+
+
+    }
+
+    //
+    public class Support
+    {
+        /// <summary>
+        /// Upper Case một kí tự tại vị trí cho trước trong một chuỗi
+        /// </summary>
+        /// <param name="origin">Chuỗi gốc</param>
+        /// <param name="foundPositon">index của kí tự cần được upper case</param>
+        /// <returns>Chuỗi mới</returns>
+        public string MyReplace(string origin, int foundPositon)
+        {
+            string result = "";
+            string temp = "";
+
+            temp = origin[foundPositon].ToString().ToUpper();
+
+            result += origin.Substring(0, foundPositon);
+            result += temp;
+            result += origin.Substring(foundPositon + 1, origin.Length - foundPositon - 1);
+
+            return result;
+        }
+    }
+
+
+    //FullnameNormalize
+    public class FullnameNormalizeOperation : StringOperation, INotifyPropertyChanged
+    {
+        public override string Name => "Fullname Normalize";
+
+        public override string Description => "This Is The Fullname Normalize method";
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public override StringOperation Clone()
+        {
+            return new FullnameNormalizeOperation();
+        }
+
+        public override void Config()
+        {
+
+        }
+
+        public override string Operation(string Origin)
+        {
+            var result = Origin.Trim();//Xóa khoảng trắng ở đầu và cuối chuỗi
+
+            //Xử lí khoảng trắng giữa các từ và kí tự đầu mỗi từ
+            result = result.ToLower();
+            string[] arrWord = result.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            result = "";
+            var temp = new Support();
+            string word;
+            for (int i = 0; i < arrWord.Length; i++)
+            {
+                word = arrWord[i].ToString();
+                word = temp.MyReplace(word, 0);
+                word += " ";
+                result += word;
+            }
+            result = result.Substring(0, result.Length - 1);
+            return result;
         }
     }
 
